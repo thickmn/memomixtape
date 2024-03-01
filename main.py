b2f3d9968fc33bbb167cc0bed9c40376816eedd7
thickmn <109317955+thickmn@users.noreply.github.com>
2024-03-01 18:35:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-fastapi_app = FastAPI()
-
 app.add_middleware(SessionMiddleware, secret_key=os.environ.get('NICEGUI_SECRET_KEY', ''))
-
 app.add_static_files('/fonts', str(Path(__file__).parent / 'src' / 'fonts'))
 app.add_static_files('/static', str(Path(__file__).parent / 'src' / 'static'))
 
@@ -23,11 +20,11 @@ app.add_static_files('/static', str(Path(__file__).parent / 'src' / 'static'))
 @user.login_page
 def login():
     user.login_form().on('success', lambda: ui.navigate.to('/'))
-
+    
 
 @user.page('/')
 async def home():
     await main_page.create()
-
+    
 
 ui.run(reload=False, dark=True, storage_secret=os.getenv('NICEGUI_SECRET_KEY'))
